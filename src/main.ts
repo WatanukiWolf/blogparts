dfce3b5c3f40214f6c11708490f85849dde97d53
WatanukWolf <watanuki.wolf.2018@gmail.com>
2022-01-25 00:53:10 +0900

    [mod] src/main.ts
    Chores

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,7 @@
 import App from './App.svelte';
-import { Account } from './class/Account';
-import { TwPost }  from './class/TwPost';
-
-let helloworld = new Account({
-    id: 'World',
-    name: 'Hello',
-    profile: 'I\'m Hello'
-});
-let post = new TwPost({ text: 'line1\nline2' });
-post.attachAccount(helloworld);
 
 const app = new App({
 	target: document.body,
-	props: {
-	    user: helloworld,
-	    post: post,
-	}
 });
 
 export default app;
