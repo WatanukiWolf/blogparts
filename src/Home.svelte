ad438311e463c481400eb1a1573b76181828c196
WatanukWolf <watanuki.wolf.2018@gmail.com>
2022-01-25 00:48:37 +0900

    [add] src/Home.svelte
    Create index page

---
+++ src/Home.svelte
@@ -0,0 +1,24 @@
+<script lang='typescript'>
+import TwPost from './TwPost.svelte';
+import TwModalWindow from './TwModalWindow.svelte';
+import type { Account } from './class/Account';
+import type { TwPost as TwPostClass } from './class/TwPost';
+
+export let user: Account;
+export let post: TwPostClass;
+
+let modalOn: boolean = false;
+
+function toggleModalWindow(e) {
+    modalOn = !modalOn; 
+}
+</script>
+
+<main>
+    <TwPost post={post} clickOnHeaderHandler={toggleModalWindow} />
+    <TwModalWindow bind:on={modalOn} user={user} />
+</main>
+
+<style>
+    
+</style>
